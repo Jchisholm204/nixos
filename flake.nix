b5be0768ac828bc5361061c7731b393995b454ad
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-12-03 12:20:59 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -2,11 +2,14 @@
     description = "NixOS Taurine Flake";
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        # helix editor, use the master branch
+        helix.url = "github:helix-editor/helix/master";
     };
 
     outputs = { self, nixpkgs, ... }@inputs: {
         nixosConfigurations.taurine = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
             modules = [
                 ./configuration.nix
             ];
