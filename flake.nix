d5cd21a583ee846ff946e169fbe4bd992df3652d
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-12-03 11:58:26 -0500

    init flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+    description = "NixOS Taurine Flake";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    };
+
+    outputs = { self, nixpkgs, ... }@inputs: {
+        nixosConfigurations.taurine = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+            ];
+        };
+    };
+}
